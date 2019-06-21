b16a3325be8bf1915570c99d6052969b6d3e6b95
David Řehoř <rehord@email.cz>
2019-06-21 10:13:42 +0200

    main.blade.php

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -36,8 +36,8 @@
 
             .top-right {
                 position: absolute;
-                right: 10px;
-                top: 18px;
+                right: 45%;
+                top: 5vmin;
             }
 
             .content {
@@ -81,17 +81,11 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Poll app
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    Login to start polling
                 </div>
             </div>
         </div>
