a3cac6a7f4bdc668dd705922a0d88c7453aa8bcf
David Řehoř <rehord@email.cz>
2019-06-21 16:25:14 +0200

    fixis¨

--- app/Http/Controllers/OptionController.php
+++ app/Http/Controllers/OptionController.php
@@ -49,7 +49,7 @@ class OptionController extends Controller
         $option->votes = 0;
         $option->save();
 
-        return redirect(action('OptionController@index'));
+        return redirect(action('OptionController@index', compact('pollid')));
     }
 
     /**
